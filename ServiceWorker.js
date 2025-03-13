d9372e3cd9873e0d496841fe9e7916ff1723e7bd
L0G1CK <brawler5731943@gmail.com>
2025-03-14 00:06:31 +0300

    resetting

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const version = "1.1";  // Меняй версию при деплое
+const version = "1.3";  // Меняй версию при деплое
 const cacheName = `RussianCryptoCat-ForceToFight-${version}`;
 const contentToCache = [
     "Build/ForceToFight.loader.js",
